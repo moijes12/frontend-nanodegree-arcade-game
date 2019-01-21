ed9559e7fe36af594b997605cfc9b709a3391893
Alexander D'Souza <adsouza@sevone.com>
2019-01-21 23:35:39 +0530

    Add Player class skeleton and render basic chars

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x = 0, y = 0) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    // The location of the Enemy
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
@@ -24,13 +27,33 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+/**
+ * @description The Player class which will inherit from Enemy.
+ */
+class Player extends Enemy {
+    /*
+     * @descriptor The constructor for the Player class.
+     */
+    constructor(x = 0, y = 0) {
+        super(x, y);
+        this.sprite = 'images/char-boy.png';
+    }
 
+    /*
+     * @description A do-nothing handle input method.
+     */
+    handleInput(keyCode) {
+        //TODO: Implementation pending
+    }
+}
 
 // Now instantiate your objects.
+let enemy1 = new Enemy(1, 1);
+let enemy2 = new Enemy(1, 100);
 // Place all enemy objects in an array called allEnemies
+let allEnemies = [enemy1, enemy2];
 // Place the player object in a variable called player
-
-
+let player = new Player(200, 400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
